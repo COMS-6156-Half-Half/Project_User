75cd448fc5563054e5ef295d45e33f08d08e44ab
王泽方 <wangzefang@ous-macbook-pro.myfiosgateway.com>
2022-12-13 23:05:58 -0500

    add_currentuser_and_response_return2

---
+++ backend/find_user.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request
+from flask import Flask, render_template, session, Blueprint, Response
+from flask_login import login_required
+import json
+from models import db, Users
+
+
+find_user = Blueprint('find_user', __name__)
+
+@find_user.route('/find_user', methods=['GET', 'POST'])
+def index():
+    data = request.get_json()
+    print(data['id'])
+    id = int(data['id'])
+    user = Users.query.filter_by(id=id).first()
+    if user:
+        return Response("User exist", status=200, content_type="text/plain")
+
+    else:
+        return Response("User not found", status=404, content_type="text/plain")
+
