919dcd46226e5741eab790d9ade1cfce01bfef81
王泽方 <wangzefang@ous-macbook-pro.myfiosgateway.com>
2022-12-15 17:00:54 -0500

    renew_get_current_id

--- backend/current_user.py
+++ backend/current_user.py
@@ -1,15 +1,24 @@
 from flask import Blueprint
-from flask import Flask, render_template, session, Blueprint, Response
+from flask import Flask, render_template, session, Blueprint, Response, request
 from flask_login import login_required
 import json
 current_user = Blueprint('current_user', __name__)
+from models import db, Users
 
-@current_user.route('/current_user')
+
+@current_user.route('/current_user', methods=['GET', 'POST'])
 def index():
-    if '_user_id' not in session:
-        return Response("User not log in", status=404, content_type="text/plain")
-    else:
-        user_id = session['_user_id']
-        result = {'user_id': user_id}
-        return Response(json.dumps(result), status=200, content_type="application.json")
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data['username']
+        user = Users.query.filter_by(username=username).first()
+        if user:
+            r = {'userid': str(user.id)}
+            print(r)
+            return Response(json.dumps(r), status=200, content_type="application.json")
+
+        else:
+            return Response("user-not-found", status=404, content_type="text/plain")
+
+
 
